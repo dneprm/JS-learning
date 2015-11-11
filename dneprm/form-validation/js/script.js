7d023468fc846d2ae5e570a9b20f3d01d9946e1e
dneprm <mariya.turko@iskyweb.com>
2015-11-11 18:17:30 +0200

    Added validation (jquery.validate)

--- dneprm/form-validation/js/script.js
+++ dneprm/form-validation/js/script.js
@@ -1,5 +1,40 @@
 $(document).ready(function(){
 	$("#telephone").mask("+99(999) 999-9999");
-    
+    $("#myForm").validate({
+
+       rules:{
+
+            mytext:{
+                required: true
+            },
+
+            telephone:{
+                required: true
+            },
+
+            email:{
+                required: true,
+                email: true
+            }
+       },
+
+       messages:{
+
+             mytext:{
+                required: "Заполните, пожалуйста, это поле"
+            },
+
+            telephone:{
+                required: "Заполните, пожалуйста, это поле"
+            },
+
+            email:{
+                required: "Заполните, пожалуйста, это поле",
+                email: "Пожалуйста, введите корректный email (например, name@email.com)"
+            }
+
+       }
+
+    });
 
 });
